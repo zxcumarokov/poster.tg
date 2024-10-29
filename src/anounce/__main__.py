ac7b76714c5c77de3b1f445c122c46bb9d772f9a
ivan <ivan@MacBook-Air-Vladimir.local>
2024-10-29 17:54:03 +0500

    docs

--- src/anounce/__main__.py
+++ src/anounce/__main__.py
@@ -1,16 +1,12 @@
+# Standard Librar
 # Standard Library
 import os
+from datetime import timedelta  # Добавляем для работы с временем
 from datetime import (
     datetime,
-    timedelta,
     timezone,
 )
-from logging import (
-    FileHandler,
-    Formatter,
-    basicConfig,
-    getLogger,
-)
+from logging import basicConfig
 
 # Third Party Stuff
 from dotenv import load_dotenv
@@ -29,51 +25,36 @@ from src.anounce.route.implementations import (
 
 from .send_anounce import SendAnounce
 
-# Загрузка переменных окружения
 load_dotenv()
-
-# Настройка логирования
-log_file = os.path.join("logs", "bot.log")
-basicConfig(
-    level="DEBUG",
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-    handlers=[
-        FileHandler(log_file),
-    ],  # Записываем в файл
-)
-logger = getLogger(__name__)
-
-# Инициализация бота
 anounce_bot = TeleBot(os.getenv("BOT_TOKEN", ""))
 if not anounce_bot:
-    logger.error("BOT_TOKEN не установлен или пуст.")
     raise ValueError("BOT_TOKEN не установлен или пуст.")
-
 calendar_url = os.getenv("CALENDAR_URL")
 calendar_id = os.getenv("CALENDAR_ID")
 url = f"{calendar_url}{calendar_id}"
 token = os.getenv("NOTION_TOKEN")
 if not token:
-    logger.error("NOTION_TOKEN не установлен или пуст.")
     raise ValueError("NOTION_TOKEN не установлен или пуст.")
-
 notion_version = "2022-02-22"
+
+# Получаем текущую дату и прибавляем один день
 current_date = datetime.now(timezone.utc)
 next_day = current_date + timedelta(days=1)
 
 notion_token = os.getenv("NOTION_TOKEN")
 if not notion_token:
-    logger.error("NOTION_TOKEN не установлен или пуст.")
     raise ValueError("NOTION_TOKEN не установлен или пуст.")
-
 notion_base_url = os.getenv("NOTION_BASE_URL")
 if not notion_base_url:
-    logger.error("NOTION_BASE_URL не установлен или пуст.")
     raise ValueError("NOTION_BASE_URL не установлен или пуст.")
-
 anounce_view = AnounceView()
 create_ride = CreateRide()
-parse_ride = RequestRide(url=url, token=token, notion_version=notion_version)
+
+parse_ride = RequestRide(
+    url=url,
+    token=token,
+    notion_version=notion_version,
+)
 create_route = CreateRoute()
 parse_route = ParseRouteData(notion_base_url=notion_base_url, notion_token=notion_token)
 
@@ -87,10 +68,9 @@ send_anounce = SendAnounce(
 )
 
 if __name__ == "__main__":
-    try:
-        logger.info("Запуск бота...")
-        # Отправляем данные на следующий день
-        send_anounce.send(date=next_day)
-        logger.info(f"Данные успешно отправлены на {next_day}.")
-    except Exception as e:
-        logger.exception("Произошла ошибка при выполнении бота.")
+    basicConfig(
+        level="DEBUG",
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    )
+    # Отправляем данные на следующий день
+    send_anounce.send(date=next_day)
--- src/anounce/anounce_view.py
+++ src/anounce/anounce_view.py
@@ -22,20 +22,20 @@ class AnounceView(AbstractView):
             raise AnounceViewNoRouteError("Route not found")
 
         message_text = (
-            f"{ride.training_name}\n"
             f"{formatted_time_utc}\n\n"
-            f"{ride.type_of_training}\n\n"
-            f"{ride.workout_app}: \n"
+            f"Тип тренировки: {ride.type_of_training}\n\n"
+            f"Название тренировки: {ride.training_name}\n"
+            f"Приложение: {ride.workout_app}: \n"
             f"Расстояние: {self._prepare_text(ride.route.distance)} км\n"
             f"Набор: {self._prepare_text(ride.route.climbing)}\n"
             f"Кофе-брейк: {self._prepare_text(ride.route.coffee_break)}\n"
             f"Сегмент дня: {self._prepare_text(ride.route.day_segment)}\n"
             f"Место старта: {self._prepare_text(ride.route.start_place)}\n\n"
-            f"{ride.voice_chanel}\n\n"
-            f"{ride.condition}\n"
-            f"{ride.manual_for_payser}\n"
-            f"{ride.gest_ures}\n\n"
-            f"{ride.post_scriptum}\n\n"
+            f"Голосовой канал: {ride.voice_chanel}\n\n"
+            f"Описание условий: {ride.condition}\n"
+            f"Мануал{ride.manual_for_payser}\n"
+            f"Жесты: {ride.gest_ures}\n\n"
+            f"PS:{ride.post_scriptum}\n\n"
             f'Маршрут: <a href="{ride.route_id}">здесь</a>\n'
         )
 
